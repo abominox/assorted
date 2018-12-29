e9558d81726fa7e0d3ce1f016947b1ca337ebf54
abominox <pwnagepal@gmail.com>
2018-12-29 04:21:02 -0500

    Testing dedicated encoder HW

--- scripts/nightly_encode.sh
+++ scripts/nightly_encode.sh
@@ -13,14 +13,14 @@ fi
 
 # All .mp4 files recursively in current dir, created in last day
 IFS=$'\n'
-for video in $(find "$@" -name "*.mp4");
+for video in $(find "$@" -mtime +3 -name "*.mp4" -o -name "*.avi" -o -name "*.mov");
 do
   # If no .mp4 files, exit
   #[ -f "${video}" ] || echo "No .mp4 files found in current dir!" && break
 
   ff_filesize=$(ffprobe -i "${video}" -show_entries format=size -v quiet -of csv="p=0")
   filesize=$(echo "scale=2; $(echo "$ff_filesize" / 1024 | bc -l)" / 1024 | bc -l)
-  ffmpeg -y -i "${video}" -threads 3 -vcodec libx264 -crf 27 TEMP.mp4 && mv TEMP.mp4 "${video}"
+  ffmpeg -y -i "${video}" -threads 8 -vcodec libx264 -crf 27 -preset veryfast -vsync 1 -async 1 -c:a copy TEMP.mp4 && mv TEMP.mp4 "${video}"
   ff_filesize_2=$(ffprobe -i "${video}" -show_entries format=size -v quiet -of csv="p=0")
   converted_size=$(echo "scale=2; $(echo "$ff_filesize_2" / 1024 | bc -l)" / 1024 | bc -l)
 
